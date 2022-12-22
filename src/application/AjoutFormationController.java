03de3271a9cff86cece8045fe8d73fcc10989d9e
ilham <arbouchilham1@gmail.com>
2022-12-22 15:15:30 +0100

    Correction

--- src/application/AjoutFormationController.java
+++ src/application/AjoutFormationController.java
@@ -8,8 +8,6 @@ import java.util.ResourceBundle;
 
 import entite.Apprenant;
 import entite.Formateur;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
